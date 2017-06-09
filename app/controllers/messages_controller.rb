d6c511b811840fcc7d7581d1da4effd7b5573eae
vanezp01 <vanezp01@gmail.com>
2017-06-09 11:31:49 -0700

    index and show action

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -1,6 +1,11 @@
 class MessagesController < ApplicationController
 
   def index
+    @messages = Message.all.order("created_at DESC")
+  end
+
+  def show
+    @message = Message.find(params[:id])
   end
 
   def new
