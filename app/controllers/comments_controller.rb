807a4fa244e2291d5667f70f0ba74ffb16a498a3
vanezp01 <vanezp01@gmail.com>
2017-06-10 06:10:45 -0700

    delete and edit functions for comments

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,9 +1,11 @@
 class CommentsController < ApplicationController
+  before_action :find_message, only: [:create, :edit, :update, :destroy]
+  before_action :find_comment, only: [:edit, :update, :destroy]
 
   def create
     @message = Message.find(params[:message_id])
     @comment = @message.comments.create(comment_params)
-    @comment.user_id = current_user.id 
+    @comment.user_id = current_user.id
 
     if @comment.save
       redirect_to message_path(@message)
@@ -12,9 +14,33 @@ class CommentsController < ApplicationController
     end
   end
 
+  def edit
+  end
+
+  def update
+    if @comment.update(comment_params)
+      redirect_to message_path(@message)
+    else
+      render 'edit'
+    end
+  end
+
+  def destroy
+    @comment.destroy
+    redirect_to message_path(@message)
+  end
+
   private
 
   def comment_params
     params.require(:comment).permit(:content)
+  end
+
+  def find_message
+    @message = Message.find(params[:message_id])
+  end
+
+  def find_comment
+    @comment = @message.comments.find(params[:id])
   end
 end
