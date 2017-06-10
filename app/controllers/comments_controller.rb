1a3ff4fa7d0f1cf2563ae89e3c0d943fa78b83bf
vanezp01 <vanezp01@gmail.com>
2017-06-10 05:43:46 -0700

    ability to comment on messages

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -3,7 +3,7 @@ class CommentsController < ApplicationController
   def create
     @message = Message.find(params[:message_id])
     @comment = @message.comments.create(comment_params)
-    @comment.user_id = current_user.user_id
+    @comment.user_id = current_user.id 
 
     if @comment.save
       redirect_to message_path(@message)
@@ -15,6 +15,6 @@ class CommentsController < ApplicationController
   private
 
   def comment_params
-    params.require(:comment).permit(:content) 
+    params.require(:comment).permit(:content)
   end
 end
